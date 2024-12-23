101a3a0d3342272ff4389ec38843e2b7a560f858
Manhattan <carra.rules@gmail.com>
2024-12-23 18:33:14 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Manhattan on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
