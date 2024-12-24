a274880e98e38f95b397074d9558e5cb8e7c6347
Manhattan <carra.rules@gmail.com>
2024-12-24 12:54:35 +0100

    Add assets images for genre and DetailView

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self, destination: { book in
+                DetailView(book: book)
+            })
             .toolbar{
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {
